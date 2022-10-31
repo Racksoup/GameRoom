8f11b2edebd2d78d4e8c50b1d49260ef98929a5b
Racksoup <rack.connor1@gmail.com>
2022-10-30 22:12:29 -0400

    bullets shoot properly, no collision

--- asteroids.lua
+++ asteroids.lua
@@ -9,17 +9,22 @@ function GR:CreateAsteroids()
   GR.Asteroids.Const.ScreenSizeX = 750
   GR.Asteroids.Const.ScreenSizeY = 500
   GR.Asteroids.Const.ColSize = 25
-
+  GR.Asteroids.Const.BulletSize = 20
+  GR.Asteroids.Const.BulletThickness = 4
+  GR.Asteroids.Const.BulletSpeed = 120
+  
   -- Asteroids Frame
   GR_GUI.Main.Asteroids = CreateFrame("Frame", Asteroids, GR_GUI.Main, "ThinBorderTemplate")
   local Asteroids = GR_GUI.Main.Asteroids
   Asteroids:SetPoint("BOTTOM", 0, 25 * (GR_GUI.Main:GetHeight() / 640))
   Asteroids:SetSize(GR_GUI.Main:GetWidth() * (GR.Asteroids.Const.ScreenSizeX / 800), GR_GUI.Main:GetHeight() * (GR.Asteroids.Const.ScreenSizeY / 640))
   Asteroids:Hide()
-
+  
   -- Variables
   GR.Asteroids.Phase = "Stopped"
   GR.Asteroids.GameTime = 0
+  GR.Asteroids.ScreenXRatio = Asteroids:GetWidth() / GR.Asteroids.Const.ScreenSizeX
+  GR.Asteroids.ScreenYRatio = Asteroids:GetHeight() / GR.Asteroids.Const.ScreenSizeY
   GR.Asteroids.ShipRotation = -.165
   GR.Asteroids.ShipSize = GR.Asteroids.Const.ShipSize
   GR.Asteroids.ShipAcceleration = GR.Asteroids.Const.ShipAcceleration
@@ -29,13 +34,15 @@ function GR:CreateAsteroids()
   GR.Asteroids.ShipYVelocity = 0
   GR.Asteroids.ShipPosX = Asteroids:GetWidth() / 2
   GR.Asteroids.ShipPosY = Asteroids:GetHeight() / 2
-  GR.Asteroids.ScreenXRatio = Asteroids:GetWidth() / GR.Asteroids.Const.ScreenSizeX
-  GR.Asteroids.ScreenYRatio = Asteroids:GetHeight() / GR.Asteroids.Const.ScreenSizeY
+  GR.Asteroids.BulletSize = GR.Asteroids.Const.BulletSize
+  GR.Asteroids.BulletThickness = GR.Asteroids.Const.BulletThickness
+  GR.Asteroids.BulletSpeed = GR.Asteroids.Const.BulletSpeed
   
   -- Create
   GR:CreateAsteroidsGameLoop()
   GR:CreateAsteroidsGameButtons()
   GR:CreateAsteroidsShip()
+  GR:CreateAsteroidsBullets()
 
   -- Size
   GR:SizeAsteroids()
@@ -68,6 +75,9 @@ function GR:CreateAsteroidsGameLoop()
     end
     if (key == "D") then
       GR.Asteroids.DownD = true
+    end
+    if (key == "SPACE") then
+      GR:AsteroidsShootBullet()
     end
   end)
   Game:SetScript("OnKeyUp", function(self, key) 
@@ -156,6 +166,27 @@ function GR:CreateAsteroidsShip()
   Ship.Line4:SetColorTexture(0,.4,1, 1)
 end
 
+function GR:CreateAsteroidsBullets()
+  local Asteroids = GR_GUI.Main.Asteroids
+  Asteroids.Bullets = {}
+  local Bullets = Asteroids.Bullets
+
+  for i=1, 4, 1 do
+    Bullets[i] = CreateFrame("Frame", nil, Asteroids)
+    Bullets[i]:SetPoint("BOTTOMLEFT", 0, 0)
+    Bullets[i]:SetSize(2,2)
+    Bullets[i].Line = Bullets[i]:CreateLine()
+    Bullets[i].Line:SetThickness(4)
+    Bullets[i].Line:SetColorTexture(1,1,1,1)
+    Bullets[i]:Hide()
+
+    Bullets[i].VelocityX = 0
+    Bullets[i].VelocityY = 0
+    Bullets[i].PosY = 0
+    Bullets[i].PosX = 0
+  end
+end
+
 -- resize
 function GR:SizeAsteroids()
   local Main = GR_GUI.Main
@@ -182,6 +213,7 @@ function GR:SizeAsteroids()
   StopBtn.FS:SetTextScale(1.8 * ((WidthRatio + HeightRatio) / 2))
   
   GR:SizeAsteroidsShip(WidthRatio, HeightRatio)
+  GR:SizeAsteroidsBullets()
 end
 
 function GR:SizeAsteroidsShip(WidthRatio, HeightRatio)
@@ -192,7 +224,6 @@ function GR:SizeAsteroidsShip(WidthRatio, HeightRatio)
   GR.Asteroids.ShipAcceleration = GR.Asteroids.Const.ShipAcceleration * ScreenRatio
   GR.Asteroids.ShipMaxSpeed = GR.Asteroids.Const.ShipMaxSpeed * ScreenRatio
 
-  print(GR.Asteroids.ShipAcceleration, GR.Asteroids.ShipMaxSpeed)
 
   Ship.Line1:SetThickness(3 * ((WidthRatio + HeightRatio) / 2))
   Ship.Line2:SetThickness(3 * ((WidthRatio + HeightRatio) / 2))
@@ -202,6 +233,19 @@ function GR:SizeAsteroidsShip(WidthRatio, HeightRatio)
 
   GR.Asteroids.ShipSize = GR.Asteroids.Const.ShipSize * ((WidthRatio + HeightRatio) / 2)
   GR:AsteroidsRotateShip(Ship)
+end
+
+function GR:SizeAsteroidsBullets()
+  local Bullets = GR_GUI.Main.Asteroids.Bullets
+  
+  GR.Asteroids.BulletSize = GR.Asteroids.Const.BulletSize * ((GR.Asteroids.ScreenXRatio + GR.Asteroids.ScreenYRatio) / 2)
+  GR.Asteroids.BulletThickness = GR.Asteroids.Const.BulletThickness * ((GR.Asteroids.ScreenXRatio + GR.Asteroids.ScreenYRatio) / 2)
+  GR.Asteroids.BulletSpeed = GR.Asteroids.Const.BulletSpeed * ((GR.Asteroids.ScreenXRatio + GR.Asteroids.ScreenYRatio) / 2)
+
+  for i=1, #Bullets, 1 do
+    Bullets[i].Line:SetThickness(GR.Asteroids.BulletThickness)
+    GR:AsteroidsPosAndRotateBullet(Bullets[i])
+  end
 end
 
 -- hide / show
@@ -227,9 +271,9 @@ end
 -- functionality
 function GR:AsteroidsGameLoop(self, elapsed)
   GR.Asteroids.GameTime = GR.Asteroids.GameTime + elapsed
-  -- print(GR.Asteroids.GameTime)
 
   GR:AsteroidsUpdateShip(elapsed)
+  GR:AsteroidsUpdateBullets(elapsed)
 end
 
 function GR:AsteroidsStartGame()
@@ -277,12 +321,65 @@ function GR:AsteroidsPauseGame()
   Asteroids.Game:Hide()
 end
 
+-- Bullet
+function GR:AsteroidsShootBullet()
+  local Bullets = GR_GUI.Main.Asteroids.Bullets
+  local BulletShot = false
+
+  for i=1, #Bullets, 1 do 
+    if (Bullets[i]:IsVisible() == false and BulletShot == false) then
+      BulletShot = true
+      Bullets[i].PosX = GR.Asteroids.ShipPosX
+      Bullets[i].PosY = GR.Asteroids.ShipPosY
+      Bullets[i].Angle = (3.14159 * ((-GR.Asteroids.ShipRotation -.66) % 4)) / 2
+      GR:AsteroidsPosAndRotateBullet(Bullets[i])
+      Bullets[i]:Show()
+      C_Timer.After(5, function()
+        Bullets[i]:Hide()
+      end)
+    end
+  end
+end
+
+function GR:AsteroidsPosAndRotateBullet(Bullet)
+  -- Rotate
+  local function RotateCoordPair (x,y,ox,oy,a,asp)
+    y=y/asp
+    oy=oy/asp
+    return ox + (x-ox)*math.cos(a) - (y-oy)*math.sin(a),
+      (oy + (y-oy)*math.cos(a) + (x-ox)*math.sin(a))*asp
+  end
+
+  local BulletSize = GR.Asteroids.BulletSize
+  local coords={tl={x=0,y=0},
+  bl={x=0,y=1},
+  tr={x=1,y=0},
+  br={x=1,y=1}}
+  local origin={x=0.5,y=1}
+  local aspect=1
+  
+  angle= (3.14159 * (GR.Asteroids.ShipRotation + 0.2)) / 2
+  local line = {}
+  line.ULx, line.ULy = RotateCoordPair(coords.tl.x,coords.tl.y,origin.x,origin.y,angle,aspect)
+  line.LLx, line.LLy = RotateCoordPair(coords.bl.x,coords.bl.y,origin.x,origin.y,angle,aspect)
+  line.URx, line.URy = RotateCoordPair(coords.tr.x,coords.tr.y,origin.x,origin.y,angle,aspect)
+  line.LRx, line.LRy = RotateCoordPair(coords.br.x,coords.br.y,origin.x,origin.y,angle,aspect)
+  Bullet.Line:SetStartPoint("CENTER", ((line.LLx + line.LRx) / 2 * BulletSize) - BulletSize / 2, ((line.LLy + line.LRy) / 2 * BulletSize) - BulletSize) 
+  Bullet.Line:SetEndPoint("CENTER", ((line.URx + line.ULx) / 2 * BulletSize) - BulletSize / 2, ((line.URy + line.ULy) / 2 * BulletSize) - BulletSize) 
+ 
+  -- Position
+  local Ship = GR_GUI.Main.Asteroids.Ship
+  local point, relativeTo, relativePoint, xOfs, yOfs = Ship:GetPoint()
+  Bullet:SetPoint("BOTTOMLEFT", xOfs, yOfs)
+
+  -- Velocity
+
+end
+
 -- Update
 function GR:AsteroidsUpdateShip(elapsed)
   local Asteroids = GR_GUI.Main.Asteroids
   local Ship = Asteroids.Ship
-
-  --Ship:SetPoint("BOTTOMLEFT", Asteroids:GetWidth() / 2 + GR.Asteroids.GameTime * 20, Asteroids:GetHeight() / 2 + GR.Asteroids.GameTime * 20)
 
   if (GR.Asteroids.DownA == true) then
     GR.Asteroids.ShipRotation = (GR.Asteroids.ShipRotation + elapsed * 2) % 4
@@ -296,7 +393,10 @@ function GR:AsteroidsUpdateShip(elapsed)
     GR:AsteroidsAccelerateShip(elapsed, Asteroids, Ship)
   end
 
+  -- speed/velocity
   GR:AsteroidsApplySpeed(elapsed, Asteroids, Ship)
+
+  -- collisions
   GR:AsteroidsColShipWall(Asteroids, Ship)
 end
 
@@ -335,7 +435,7 @@ function GR:AsteroidsApplySpeed(elapsed, Asteroids, Ship)
 end
 
 function GR:AsteroidsRotateShip(Ship)
-  function RotateCoordPair (x,y,ox,oy,a,asp)
+  local function RotateCoordPair (x,y,ox,oy,a,asp)
     y=y/asp
     oy=oy/asp
     return ox + (x-ox)*math.cos(a) - (y-oy)*math.sin(a),
@@ -395,6 +495,18 @@ function GR:AsteroidsRotateShip(Ship)
   Ship.Col:SetPoint("CENTER", ((line4.LLx + line4.LRx) / 2 * ShipSize) - ShipSize / 2, ((line4.LLy + line4.LRy) / 2 * ShipSize) - ShipSize)
 end
 
+function GR:AsteroidsUpdateBullets(elapsed)
+  local Bullets = GR_GUI.Main.Asteroids.Bullets
+
+  for i=1, #Bullets, 1 do
+    if (Bullets[i]:IsVisible()) then
+      Bullets[i].PosY = Bullets[i].PosY + (math.cos(Bullets[i].Angle) - math.sin(Bullets[i].Angle)) * elapsed * GR.Asteroids.BulletSpeed
+      Bullets[i].PosX = Bullets[i].PosX + (math.cos(Bullets[i].Angle) + math.sin(Bullets[i].Angle)) * elapsed * GR.Asteroids.BulletSpeed
+      Bullets[i]:SetPoint("BOTTOMLEFT", Bullets[i].PosX, Bullets[i].PosY)
+    end
+  end
+end
+
 -- Collision
 function GR:AsteroidsColShipWall(Asteroids, Ship)
   local point, relativeTo, relativePoint, xOfs, yOfs = Ship:GetPoint()
@@ -416,22 +528,18 @@ function GR:AsteroidsColShipWall(Asteroids, Ship)
   -- ship right past border left
   if (Shipx.URx < Border.LLx) then 
     GR.Asteroids.ShipPosX = Asteroids:GetWidth() - GR.Asteroids.ShipSize
-    print('Left')
   end
   -- ship left past border right
   if (Shipx.LLx > Border.URx) then 
     GR.Asteroids.ShipPosX = 0 + GR.Asteroids.ShipSize
-    print('Right')
   end
   -- ship top past border bottom
   if (Shipx.URy < Border.LLy) then 
     GR.Asteroids.ShipPosY = Asteroids:GetHeight() - GR.Asteroids.ShipSize
-    print('Bottom')
   end
   -- ship bottom past border top
   if (Shipx.LLy > Border.URy) then 
     GR.Asteroids.ShipPosY = 0 + GR.Asteroids.ShipSize
-    print('Top')
   end
 end
 
