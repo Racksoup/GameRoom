f38ed5e114555b01799a1d74c89e9e480c153c86
racksoup <rack.connor1@gmail.com>
2024-05-21 15:09:03 -0400

    suduko button grid

--- suduko.lua
+++ suduko.lua
@@ -3,12 +3,48 @@ function GR:CreateSuduko()
   local Main = GR_GUI.Main
   GR.Suduko = {}
   GR.Suduko.Const = {}
+  GR.Suduko.Const.NumOfCols = 9
+  GR.Suduko.Const.NumOfRows = 9
 
   Main.Suduko = CreateFrame("Frame", "Suduko", Main, "ThinBorderTemplate")
   Main.Suduko:Hide()
 
-  
+  GR:CreateSudukoGrid()
+  GR:CreateSudukoBlackLines()
+
   GR:SizeSuduko()
+end
+
+function GR:CreateSudukoGrid()
+  local Main = GR_GUI.Main
+  local Suduko = Main.Suduko
+  local rows = GR.Suduko.Const.NumOfRows
+  local cols = GR.Suduko.Const.NumOfCols
+
+  Suduko.Grid = {}
+
+  for i = 1, rows, 1 do
+    for j = 1, cols, 1 do
+      Suduko.Grid[j + ((i -1) * cols)] = CreateFrame("BUTTON", nil, Suduko, "ThinBorderTemplate")
+      local Tile = Suduko.Grid[j + ((i -1) * cols)] 
+      Tile.FS = Tile:CreateFontString(nil, "OVERLAY", "GameTooltipText")
+      Tile.FS:Hide()
+      Tile:RegisterForClicks("LeftButtonDown", "RightButtonDown")
+      Tile:SetScript("OnClick", function(self, button, down)
+        if (button == "LeftButton") then
+          
+        end
+
+        if (button == "RightButton") then
+
+        end
+      end)
+    end
+  end
+end
+
+function GR:CreateSudukoBlackLines()
+
 end
 
 -- Size
@@ -18,6 +54,33 @@ function GR:SizeSuduko()
 
   Suduko:SetPoint("BOTTOM", 0, 25 * Main.YRatio)
   Suduko:SetSize(GR.Win.Const.SudukoScreenWidth * Main.XRatio, GR.Win.Const.SudukoScreenHeight * Main.YRatio)
+
+  GR:SizeSudukoGrid()
+  GR:SizeSudukoBlackLines()
+end
+
+function GR:SizeSudukoGrid()
+  local Main = GR_GUI.Main
+  local Suduko = Main.Suduko
+  local Grid = Suduko.Grid
+  local cols = GR.Suduko.Const.NumOfCols
+  local rows = GR.Suduko.Const.NumOfRows
+  local height = GR.Win.Const.SudukoScreenHeight
+  local width = GR.Win.Const.SudukoScreenWidth
+  
+  for i = 1, rows, 1 do
+    for j = 1, cols, 1 do
+      local Tile = Suduko.Grid[j + ((i - 1) * cols)]
+      Tile:SetPoint("BOTTOMLEFT", (width * Main.XRatio) * ((j -1) / cols), (height * Main.YRatio) * ((i -1) / rows))
+      Tile:SetSize((width * Main.XRatio) / cols, (height * Main.YRatio) / rows)
+      Tile.FS:SetPoint("CENTER")
+      Tile.FS:SetTextScale(1 * Main.ScreenRatio)
+    end
+  end
+end
+
+function GR:SizeSudukoBlackLines()
+
 end
 
 -- Show / Hide
